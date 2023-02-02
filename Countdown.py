0abbba3b20a22f2b770699186ee8c99b662becc8
Aman Pandit <amanpandit3027@gmail.com>
2023-02-02 07:42:04 +0530

    Update Countdown.py
    updated the code

--- Countdown.py
+++ Countdown.py
@@ -1,1 +1,15 @@
+# Countdown.
+
+import time
+
+# Set the number of seconds for the Stopwatch or Countdown timer.
+seconds = 10
+
+# Stopwatch or Countdown timer.
+while seconds > 0:
+    print(seconds)
+    time.sleep(1)  # delay of  1 second.
+    seconds -= 1
+
+print("You ran out of time")
 
